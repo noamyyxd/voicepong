956a4e2dd04eb600bc37bbcf294a2859c2dcc474
noamyyxd <noam.vaisbuch@gmail.com>
2019-05-29 02:36:41 +0300

    Added db update script and an update for the lyrics excel

---
+++ dbupdate/dbupdate/DBConnect.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbupdate
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "remotemysql.com";
+            database = "sxZaoaM2jp";
+            uid = "sxZaoaM2jp";
+            password = "GkVgSXYqGS";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        throw new Exception("Cannot connect to server. ");
+
+                    case 1045:
+                        throw new Exception("Invalid username/password, please try again");
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //Insert statement
+        public void NonQuery(string sql)
+        {
+            string query = sql;
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                try
+                {
+                    //create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    //Execute command
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex) {
+                    this.CloseConnection();
+                    throw ex;
+                }
+                //close connection
+                this.CloseConnection();
+            }
+        }
+    }
+}
---
+++ dbupdate/dbupdate/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace dbupdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect conn = new DBConnect();
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"..\voicepong\LyricsData.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            string id = null, artist = null, title = null, lyrics = null, sql = null;
+            int cFaults = 0;
+            for (int i = 1; i <= rowCount; i++)
+            {
+                //write the value to the console
+                if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                    id = xlRange.Cells[i, 1].Value2.ToString();
+
+                if (xlRange.Cells[i, 2] != null && xlRange.Cells[i, 2].Value2 != null)
+                    artist = xlRange.Cells[i, 2].Value2.ToString();
+
+                if (xlRange.Cells[i, 3] != null && xlRange.Cells[i, 3].Value2 != null)
+                    title = xlRange.Cells[i, 3].Value2.ToString();
+
+                if (xlRange.Cells[i, 4] != null && xlRange.Cells[i, 4].Value2 != null)
+                    lyrics = xlRange.Cells[i, 4].Value2.ToString();
+
+                if (id != null && artist != null && title != null && lyrics != null && i > 27)
+                {
+                    sql = "INSERT INTO `Songs` VALUES('" + id + "', \"" + artist + "\", \"" + title + "\", \"" + lyrics + "\")";
+                    try
+                    {
+                        conn.NonQuery(sql);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Could not parse id:" + id);
+                        cFaults++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Ended with " + cFaults + " faults.");
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        }
+    }
+}
